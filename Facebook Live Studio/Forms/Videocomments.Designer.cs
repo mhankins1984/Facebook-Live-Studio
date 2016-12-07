fe39e66888bf288d6654f09891749f129368ef9c
mhankins84 <matthew.hankins@itv.com>
2016-12-07 13:42:41 +0000

    Commit IDs

--- Facebook Live Studio/Forms/Videocomments.Designer.cs
+++ Facebook Live Studio/Forms/Videocomments.Designer.cs
@@ -23,6 +23,7 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -37,11 +38,21 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Videocomments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(864, 545);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Videocomments";
@@ -53,5 +64,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
